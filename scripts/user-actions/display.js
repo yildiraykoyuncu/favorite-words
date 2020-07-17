c4348f0de0f87d43ed84c955c0469b1bdeb17dc3
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-07-17 17:01:37 +0200

    Fixed typo
    I changed it. Thanks

--- scripts/user-actions/display.js
+++ scripts/user-actions/display.js
@@ -3,7 +3,7 @@ console.log('--- loading: display-all.js');
 
 // reverse engineering!
 //   the handler has no tests, study the demo to write this function
-const displayHandler = (message = 'All words:', arrayToDisplay = words) => {
+const displayHandler = (message = 'all words:', arrayToDisplay = words) => {
     debugger;
     const combineStrings = (acc, next) => {
         return acc + '\n' + next;
