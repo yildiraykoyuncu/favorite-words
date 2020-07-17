4c3ecfc216b8a86ad0160dd322b44658ee553545
Aleksandra <alex.shalimova@gmail.com>
2020-07-17 16:43:01 +0200

    Fixed add-word Closes gh-2

--- scripts/user-actions/add-word.js
+++ scripts/user-actions/add-word.js
@@ -1,5 +1,6 @@
 'use strict';
-console.log('--- loading: add-word.js');
+
+/* console.log('--- loading: add-word.js');
 
 const addWordHandler = () => {
   debugger;
@@ -11,4 +12,19 @@ const addWordHandler = () => {
   words._(_);
   const newMessage = ``;
   displayHandler(newMessage);
+}; */
+
+'use strict';
+console.log('--- loading: add-word.js');
+
+const addWordHandler = () => {
+  debugger;
+  const userInput = prompt('add a new word:');
+  if (userInput === null) {
+    return;
+  } else {
+    words.push(userInput);
+    const newMessage = `"${userInput}" has been added:\n`;
+    displayHandler(newMessage);
+  }
 };
